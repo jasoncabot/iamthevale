f810f4489aaf07dfc46162c72af3d42d4cb36d18
Jason Cabot <Jason.Cabot@microsoft.com>
2020-07-03 14:26:37 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [textIndex, setTextIndex] = useState(0);
+  const [textItems] = useState(['the vale', 'a person']);
+
+  const animatedText = useRef(null);
+
+  useEffect(() => {
+    const element = animatedText.current;
+    const transitionToNextItem = () => {
+      if (textIndex === textItems.length - 1) {
+        setTextIndex(0);
+      } else {
+        setTextIndex(textIndex + 1);
+      }
+    };
+    element.addEventListener("animationiteration", transitionToNextItem, false);
+    return () => { element.removeEventListener("animationiteration", transitionToNextItem); }
+  }, [animatedText, textIndex, textItems.length]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header className="masthead mb-sm">
+        <div className="inner">
+          <nav className="nav nav-masthead justify-content-center">
+            <a className="nav-link active" href="/">Home</a>
+            <a className="nav-link" href="/about">About</a>
+          </nav>
+        </div>
       </header>
+
+      <main role="main" className="inner content mt-auto">
+        <h1 className="">I am&nbsp;<span className="iam" ref={animatedText}>{textItems[textIndex]}</span></h1>
+        <div className="mt-5">
+          <p className="text-muted">Create your own and share your story</p>
+          <a href="/add" className="btn btn-lg btn-primary mr-2">I am ...</a>
+          <a href="/about" className="btn btn-lg btn-secondary">Learn more</a>
+        </div>
+      </main>
+
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>A digital inclusion project made with <span role="img" aria-label="love">❤️</span></p>
+        </div>
+      </footer>
     </div>
   );
 }
